b2350dfa36e90c6070c12773bb707be1cfea82ec
Jake Romer <mail@jakeromer.com>
2020-07-19 10:45:23 -0400

    Order products deterministically

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,10 @@
 
 class HomeController < ApplicationController
   def index
-    @products = Product.all.decorate.as_json(
-      only: %i[id name rating],
-      methods: %i[price image_url star_url],
-    )
+    @products =
+      Product.all.order(created_at: :asc).decorate.as_json(
+        only: %i[id name rating],
+        methods: %i[price image_url star_url],
+      )
   end
 end
