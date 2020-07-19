3b47b0bbdc54003df6ed86bba1e153343e5fe23e
Jake Romer <mail@jakeromer.com>
2020-07-19 10:45:16 -0400

    Add new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,20 +12,28 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: "Product was successfully created." }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.save
+      flash[:notice] = "Created: #{@product.name}"
+    else
+      flash[:alert] = @product.errors.full_messages.to_sentence
     end
+
+    render :new
   end
 
   private
 
   def product_params
-    params.fetch(:product, {})
+    product = params.require(:product).permit(:name, :rating, :price, :image)
+    product.merge(price: to_cents(product[:price]))
+  end
+
+  CURRENCY_VALUE = /[[:digit:]]+.?[[:digit:]]*/.freeze
+
+  def to_cents(dollar_amount)
+    value = dollar_amount[CURRENCY_VALUE].to_s
+    return value.to_i unless value.match?(/\./)
+
+    (value.to_f * 100).to_i
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,6 @@
 class Product < ApplicationRecord
   validates :name, :price, presence: true
   validates :rating, numericality: { in: 1..5 }
+  validates :price, numericality: { greater_than: 0 }
   has_one_attached :image
 end
