d6da89cc86ee4c474f8466303d2064453265fe80
Jake Romer <mail@jakeromer.com>
2020-07-19 10:45:17 -0400

    Style Components

--- app/javascript/packs/product_listing.jsx
+++ app/javascript/packs/product_listing.jsx
@@ -1,31 +1,74 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
 import Container from '@material-ui/core/Container'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
+    margin: '20px auto'
   },
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary
+  productRoot: {
+    flexGrow: 1,
+    padding: 10
+  },
+  media: {
+    height: 250
+  },
+  separator: {
+    borderBottom: '1px solid rgba(88, 88, 88, 0.31)',
+    borderBottomWidth: 0.1,
+    margin: 10
+  },
+  content: {
+    padding: 5,
+    textAlign: 'center'
+  },
+  bold: {
+    fontWeight: 500
+  },
+  card: {
+    margin: 0.5
   }
 }))
 
 function Product ({ product }) {
+  const classes = useStyles()
+
   return (
-    <div>
-      <img alt={product.name} src={product.image_url} />
-      <div>{product.name}</div>
-      <div>{product.price}</div>
-      <div>{product.rating}</div>
-    </div>
+    <Card className={classes.productRoot}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={product.image_url}
+          title={product.name}
+        />
+        <div className={classes.separator} />
+        <CardContent className={classes.content}>
+          <Typography
+            className={classes.bold}
+            variant='body1'
+            color='textSecondary'
+            component='p'
+          >
+            {product.name}
+          </Typography>
+          <Typography variant='body1' color='textSecondary' component='p'>
+            {product.price}
+          </Typography>
+          <Typography variant='body1' color='textSecondary' component='p'>
+            {product.rating}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   )
 }
 
@@ -33,12 +76,10 @@ function ProductRow ({ products }) {
   const classes = useStyles()
 
   return (
-    <Grid container item xs={12} spacing={3}>
+    <Grid container item xs={12} spacing={3} className={classes.card}>
       {products.map(product => (
         <Grid item xs={4} key={product.id}>
-          <Paper className={classes.paper}>
-            <Product product={product} />
-          </Paper>
+          <Product product={product} />
         </Grid>
       ))}
     </Grid>
@@ -52,14 +93,11 @@ function ProductListing ({ products }) {
     <>
       <CssBaseline />
       <Container maxWidth='md'>
-        <Typography component='div' style={{ height: '10vh' }} />
-        <div className={classes.root}>
-          <Grid container spacing={1}>
-            <ProductRow products={products.slice(0, 3)} />
-            <ProductRow products={products.slice(3, 6)} />
-            <ProductRow products={products.slice(6, 9)} />
-          </Grid>
-        </div>
+        <Grid container spacing={1} className={classes.root}>
+          <ProductRow products={products.slice(0, 3)} />
+          <ProductRow products={products.slice(3, 6)} />
+          <ProductRow products={products.slice(6, 9)} />
+        </Grid>
       </Container>
     </>
   )
